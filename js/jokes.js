4f6fc2be2a02e2315caa28e3947863998348f97a
Morgan Henning <megannh6349@gmail.com>
2022-01-07 14:05:32 -0500

    rearranged file structure, finished first draft of project

---
+++ js/jokes.js
@@ -0,0 +1,326 @@
+let jokes = [
+    {
+        joke: "How many telemarketers does it take to change a light bulb?",
+        answer: "Only one, but he has to do it while you are eating dinner."
+    },
+    {
+        joke: "How many narcissists does it take to screw in a light bulb?",
+        answer: "One. The narcissist holds the light bulb while the rest of the world revolves around him."
+    },
+    {
+        joke: "How many DIY buffs does it take to change a light bulb?",
+        answer: "One, but it takes two weeks and four trips to the hardware store."
+    },
+    {
+        joke: "How many paranoid people does it take to change a light bulb?",
+        answer: "Wait why? Who wants to know?"
+    },
+    {
+        joke: "I read that by law you must turn on your headlights when it's raining in Sweden",
+        answer: "...but how am I supposed to know when it is raining in Sweden?"
+    },
+    {
+        joke: "I was addicted to doing the hokey pokey.",
+        answer: "Don't worry, I turned myself around."
+    },
+    {
+        joke: "I don't trust stairs.",
+        answer: "They are always up to something."
+    },
+    {
+        joke: "Today, my son asked, “Can I have a bookmark?”",
+        answer: "I burst into tears—11 years old and he still doesn't know my name is Brian."
+    },
+    {
+        joke: "I remember as a kid, my dad got fired from his job as a road worker for theft.",
+        answer: "I refused to believe he could do such a thing, but when I got home, the signs were all there."
+    },
+    {
+        joke: "Why didn't Han Solo enjoy his steak dinner?",
+        answer: "It was Chewie."
+    },
+    {
+        joke: "Why don't pirates take a bath before they walk the plank?",
+        answer: "They just wash up on shore."
+    },
+    {
+        joke: "Yesterday, my mom asked if I wanted to go to the beach.",
+        answer: "I said shore."
+    },
+    {
+        joke: "Why do you never see elephants hiding in trees?",
+        answer: "Because they're so good at it."
+    },
+    {
+        joke: "Did you hear about the racing snail who got rid of his shell?",
+        answer: "He thought it would make him faster, but it just made him sluggish."
+    },
+    {
+        joke: "A turtle is crossing the road when he's mugged by two rabbits.",
+        answer: "When the police ask him what happened, the shaken turtle replies, “I don't know. It all happened so fast.”"
+    },
+    {
+        joke: "We all know about Murphy's Law: anything that can go wrong will go wrong. But have you heard of Cole's Law?",
+        answer: "It's thinly sliced cabbage."
+    },
+    {
+        joke: "When does a joke become a “dad joke?”",
+        answer: "When it becomes apparent."
+    },
+    {
+        joke: "Did you hear about the kidnapping at school?",
+        answer: "The teacher woke him up."
+    },
+    {
+        joke: "What's the least spoken language in the world?",
+        answer: "Sign language."
+    },
+    {
+        joke: "What do you call a hippie's wife?",
+        answer: "Mississippi."
+    },
+    {
+        joke: "I sold my vacuum cleaner recently.",
+        answer: "It was nice, but it was just gathering dust."
+    },
+    {
+        joke: "Have you heard about the crab politician?",
+        answer: "Turns out he's just been promoting his own shellfish interests."
+    },
+    {
+        joke: "What kind of eggs did the evil chicken lay?",
+        answer: "Deviled eggs."
+    },
+    {
+        joke: "Did you hear they arrested the devil?",
+        answer: "Yeah, they got him on possession."
+    },
+    {
+        joke: "A friend of mine didn't pay his exorcist.",
+        answer: "He got repossessed."
+    },
+    {
+        joke: "How do you make holy water?",
+        answer: "You boil the hell out of it."
+    },
+    {
+        joke: "What sound does a witch's car make?",
+        answer: "Broom broom!"
+    },
+    {
+        joke: "I want to go on record that I support farming.",
+        answer: "As a matter of fact, you could call me protractor."
+    },
+    {
+        joke: "What's the best way to watch a fly-fishing tournament?",
+        answer: "Live stream."
+    },
+    {
+        joke: "How do you tell the difference between an alligator and a crocodile?",
+        answer: "You will see one later and one in a while."
+    },
+    {
+        joke: "Why did the man name his dogs Rolex and Timex?",
+        answer: "Because they were watch dogs."
+    },
+    {
+        joke: "What do you call a dog that can do magic?",
+        answer: "A Labracabrador."
+    },
+    {
+        joke: "Why do dogs float in water?",
+        answer: "Because they are good buoys."
+    },
+    {
+        joke: "What happens when it rains cats and dogs?",
+        answer: "You have to be careful not to step in a poodle."
+    },
+    {
+        joke: "What do you call 50 pigs and 50 deer?",
+        answer: "100 sows and bucks."
+    },
+    {
+        joke: "Why do cows wear bells?",
+        answer: "Because their horns don't work."
+    },
+    {
+        joke: "What do you call a fish with no eye?",
+        answer: "A fsh."
+    },
+    {
+        joke: "Police arrested a bottle of water because it was wanted in three different states:",
+        answer: "Solid, liquid, and gas."
+    },
+    {
+        joke: "What do you call a lazy kangaroo?",
+        answer: "A pouch potato."
+    },
+    {
+        joke: "Why is grass so dangerous?",
+        answer: "Because it's full of blades."
+    },
+    {
+        joke: "A couple of cups of yogurt walk into a country club.",
+        answer: "&#34We don't serve your kind here,&#34 the bartender says. &#34Why not?&#34 one yogurt asks. &#34We're cultured.&#34"
+    },
+    {
+        joke: "A ham sandwich walks into a bar and orders a beer.",
+        answer: "The bartender says, &#34Sorry, we don't serve food here.&#34"
+    },
+    {
+        joke: "A brain walks into a bar and takes a seat.",
+        answer: "&#34I'd like some wings and a pint of beer, please,&#34 he says. &#34Sorry, but I can't serve you,&#34 the bartender replies. &#34You're out of your head.&#34"
+    },
+    {
+        joke: "A pirate walks into a bar with a paper towel on his head.",
+        answer: "The bartender says, &#34What's with the paper towel?&#34 The pirate says, &#34Arrr! I've got a Bounty on me head!&#34"
+    },
+    {
+        joke: "A guy walks into a bar and there's a horse serving drinks.",
+        answer: "The horse asks, &#34What are you staring at? Haven't you ever seen a horse tending bar before?&#34 The guy says, &#34It's not that. I just never thought the parrot would sell the place.&#34"
+    },
+    {
+        joke: "Why did Beethoven get rid of his chickens?",
+        answer: "All they said was, &#34Bach, Bach, Bach...&#34"
+    },
+    {
+        joke: "What did the DNA say to the other DNA?",
+        answer: "&#34Do these genes make me look fat?&#34"
+    },
+    {
+        joke: "What do you need to make a small fortune on Wall Street?",
+        answer: "A large fortune."
+    },
+    {
+        joke: "How does the man in the moon get his hair cut?",
+        answer: "Eclipse it"
+    },
+    {
+        joke: "Did you hear about the restaurant on the moon?",
+        answer: "Great food, but no atmosphere!"
+    },
+    {
+        joke: "Did you hear the one about the kid who started a business tying shoelaces on the playground?",
+        answer: "It was a knot-for-profit."
+    },
+    {
+        joke: "My kid wants to invent a pencil with an eraser on each end.",
+        answer: "I support him, but I just don't see the point."
+    },
+    {
+        joke: "A cheese factory exploded in France.",
+        answer: "Da brie is everywhere!"
+    },
+    {
+        joke: "Did you hear the rumor about butter?",
+        answer: "Well, I'm not going to spread it!"
+    },
+    {
+        joke: "Why do melons have weddings?",
+        answer: "Because they cantaloupe."
+    },
+    {
+        joke: "What do you call a fake noodle?",
+        answer: "An impasta."
+    },
+    {
+        joke: "What does a mobster buried in cement soon become?",
+        answer: "A hardened criminal."
+    },
+    {
+        joke: "What do you get when you cross a polar bear with a seal?",
+        answer: "A polar bear."
+    },
+    {
+        joke: "What do you call a bear with no teeth?",
+        answer: "A gummy bear."
+    },
+    {
+        joke: "If athletes get athlete's foot, what do astronauts get?",
+        answer: "Missile toe."
+    },
+    {
+        joke: "My wife asked me to sync her phone...",
+        answer: "So I threw it into the ocean."
+    },
+    {
+        joke: "My wife is really mad that I have no sense of direction.",
+        answer: "I packed up my stuff and right."
+    },
+    {
+        joke: "What did one cannibal say to the other while they were eating a clown?",
+        answer: "Does this taste funny to you?"
+    },
+    {
+        joke: "Do I enjoy making courthouse puns?",
+        answer: "Guilty."
+    },
+    {
+        joke: "What do you call someone with no body and no nose?",
+        answer: "Nobody knows."
+    },
+    {
+        joke: "You know, people say they pick their nose.",
+        answer: "I feel like I was just born with mine."
+    },
+    {
+        joke: "In a freak accident today, a photographer was killed when a huge lump of cheddar landed on him.",
+        answer: "To be fair, the people who were being photographed did try to warn him."
+    },
+    {
+        joke: "Can February March?",
+        answer: "No, but April May."
+    },
+    {
+        joke: "Not sure if you have noticed, but I love bad puns.",
+        answer: "That's just how eye roll."
+    },
+    {
+        joke: "What's a bad wizard's favorite computer program?",
+        answer: "Spell-check."
+    },
+    {
+        joke: "I was just reminiscing about the beautiful herb garden I had when I was growing up.",
+        answer: "Good thymes."
+    },
+    {
+        joke: "Today, I started reading a horror novel in Braille.",
+        answer: "Something bad is about to happen, I can feel it."
+    },
+    {
+        joke: "Why do pumpkins sit on porches?",
+        answer: "They have no hands to knock on the door."
+    },
+    {
+        joke: "My friend wants to become an archaeologist, but I'm trying to put him off.",
+        answer: "I'm convinced his life will be in ruins."
+    },
+    {
+        joke: "I got hit in the head with a can of Diet Coke today.",
+        answer: "Don't worry, I'm not hurt. It was a soft drink."
+    },
+    {
+        joke: "Cooking out this weekend? Don't forget the pickle",
+        answer: "It's kind of a big dill."
+    },
+    {
+        joke: "Justice is a dish best served cold.",
+        answer: "If it were served warm, it would be justwater."
+    },
+    {
+        joke: "What's orange and sounds like a parrot?",
+        answer: "A carrot."
+    },
+    {
+        joke: "What's the difference between a poorly dressed man on a tricycle and a well-dressed man on a bicycle?",
+        answer: "Attire."
+    },
+    {
+        joke: "Why did the raisin go out with the prune?",
+        answer: "Because he couldn't find a date."
+    },
+    {
+        joke: "What's brown and sticky?",
+        answer: "A stick."
+    }
+];
